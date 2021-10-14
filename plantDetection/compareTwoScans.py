1beefbe59dbdcd7afbac8a2c9e6d0ec9bbfd289b
dave1b <57140925+dave1b@users.noreply.github.com>
2021-10-14 23:17:11 +0200

    added comments to the functions

--- plantDetection/compareTwoScans.py
+++ plantDetection/compareTwoScans.py
@@ -1,10 +1,11 @@
+# compares two arrays and return the first machting item
 def findFirstMatchingPlantInScan(scan1, scan2):
     matchingPlant = list(set(scan1)&set(scan2))
     return matchingPlant[0]
-
+# compares two arrays for matches and return an array with all matches
 def findMatchingPlantInScan(scan1, scan2):
     matchingPlant = list(set(scan1)&set(scan2))
     return matchingPlant
-
+# compares two arrays for matches and returns boolean if there is at least one match
 def hasMatchingPlantInScan(scan1, scan2):
     return (len(findMatchingPlantInScan(scan1,scan2)) >= 0)    
