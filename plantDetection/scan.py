98bd063baea48b582943cbb13a76f9f0ad24bf2a
dave1b <57140925+dave1b@users.noreply.github.com>
2021-10-14 23:18:00 +0200

    changed name of the function

--- plantDetection/scan.py
+++ plantDetection/scan.py
@@ -1,8 +1,8 @@
-import requestAPIplantID as api
+import requestAPIplantID
 from time import gmtime, strftime
 
 class Scan():
     def __init__(self, imagePfad):
         self.imagePfad = imagePfad
-        self.possiblePlantsList = api.scan(imagePfad)
+        self.possiblePlantsList = requestAPIplantID.scan(imagePfad)
         self.timeStamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
