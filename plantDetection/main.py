84b80476124452cfe336be6340501233e4a276ce
dave1b <57140925+dave1b@users.noreply.github.com>
2021-10-16 19:41:06 +0200

    added main function

--- plantDetection/main.py
+++ plantDetection/main.py
@@ -2,20 +2,25 @@ import time
 from scan import Scan
 from compareTwoScans import hasMatchingPlantInScan, findMatchingPlantInScan, findFirstMatchingPlantInScan
 
-# Demonstration of functionality
-photo1 = "./plantDetection/photos/minth1.jpg"
-photo2 = "./plantDetection/photos/minth2.jpg"
+def main():
 
-firstScanReady = input("Erstes Pflanze gescannt?")
-ScanOfFirstPhoto = Scan(photo1)
+    # Demonstration of functionality
+    photo1 = "./plantDetection/photos/minth1.jpg"
+    photo2 = "./plantDetection/photos/minth2.jpg"
 
-secondScanReady = input("Zweite Pflanze gescannt?")
-ScanOfSecondPhoto = Scan(photo2)
+    firstScanReady = input("Erstes Pflanze gescannt?")
+    ScanOfFirstPhoto = Scan(photo1)
 
-for i in range(0,2):
-    print("comparing both photos...")
-    time.sleep(0.5)
+    secondScanReady = input("Zweite Pflanze gescannt?")
+    ScanOfSecondPhoto = Scan(photo2)
 
-print("Has found a match: ", hasMatchingPlantInScan(ScanOfFirstPhoto.possiblePlantsList,ScanOfSecondPhoto.possiblePlantsList))
-print("Found match: ", findFirstMatchingPlantInScan(ScanOfFirstPhoto.possiblePlantsList, ScanOfSecondPhoto.possiblePlantsList))
-#print(findMatchingPlantInScan(scanOnTheRoad.possiblePlantsList, scanAtEnd1.possiblePlantsList))
+    for i in range(0,3):
+        print("comparing both photos...")
+        time.sleep(0.5)
+
+    print("Has found a match: ", hasMatchingPlantInScan(ScanOfFirstPhoto.possiblePlantsList,ScanOfSecondPhoto.possiblePlantsList))
+    print("Found match: ", findFirstMatchingPlantInScan(ScanOfFirstPhoto.possiblePlantsList, ScanOfSecondPhoto.possiblePlantsList))
+    #print(findMatchingPlantInScan(scanOnTheRoad.possiblePlantsList, scanAtEnd1.possiblePlantsList))
+
+if __name__ == '__main__':
+    main()
