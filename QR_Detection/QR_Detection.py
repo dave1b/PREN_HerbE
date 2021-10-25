bfa6a34e68c3057693cd7eb87c6528fa524449c0
dave1b <57140925+dave1b@users.noreply.github.com>
2021-10-25 10:25:03 +0200

    added resolution settings

--- QR_Detection/QR_Detection.py
+++ QR_Detection/QR_Detection.py
@@ -2,6 +2,8 @@ import cv2
 
 # set up camera object
 cap = cv2.VideoCapture(0)
+#cap.set(3, 640)  # Set horizontal resolution
+#cap.set(4, 480)  # Set vertical resolution
 
 # QR code detection object
 detector = cv2.QRCodeDetector()
@@ -28,3 +30,25 @@ while True:
 # free camera object and exit
 cap.release()
 cv2.destroyAllWindows()
+
+
+cap = cv2.VideoCapture(0)
+
+def make_1080p():
+    cap.set(3, 1920)
+    cap.set(4, 1080)
+
+def make_720p():
+    cap.set(3, 1280)
+    cap.set(4, 720)
+
+def make_480p():
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+def change_res(width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+#make_720p()
+#change_res(1280, 720)
