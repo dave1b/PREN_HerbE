1e0707c968ba66d8b0ee080eac9729d28088d7ea
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2021-10-29 11:28:40 +0200

    new file

--- QR_Detection/QR_Detection1.py
+++ QR_Detection/QR_Detection1.py
@@ -1,5 +1,20 @@
 import cv2
 
+def make_1080p():
+    cap.set(3, 1920)
+    cap.set(4, 1080)
+
+def make_720p():
+    cap.set(3, 1280)
+    cap.set(4, 720)
+
+def make_480p():
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+def change_res(width, height):
+    cap.set(3, width)
+    cap.set(4, height)
 # set up camera object
 cap = cv2.VideoCapture(0)
 #cap.set(3, 640)  # Set horizontal resolution
@@ -34,21 +49,6 @@ cv2.destroyAllWindows()
 
 cap = cv2.VideoCapture(0)
 
-def make_1080p():
-    cap.set(3, 1920)
-    cap.set(4, 1080)
-
-def make_720p():
-    cap.set(3, 1280)
-    cap.set(4, 720)
-
-def make_480p():
-    cap.set(3, 640)
-    cap.set(4, 480)
-
-def change_res(width, height):
-    cap.set(3, width)
-    cap.set(4, height)
 
 #make_720p()
 #change_res(1280, 720)
