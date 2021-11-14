518d5debae47d15a0b870865bdce9471a1999d5a
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2021-11-14 20:26:42 +0100

    removed sleep (more efficient)

--- Ultrasonic/distanceWithUltrasonic.py
+++ Ultrasonic/distanceWithUltrasonic.py
@@ -19,10 +19,7 @@ def distance():
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
- 
+  
     # save StartTime
     while GPIO.input(GPIO_ECHO) == 0:
         StartTime = time.time()
@@ -30,23 +27,20 @@ def distance():
     # save time of arrival
     while GPIO.input(GPIO_ECHO) == 1:
         StopTime = time.time()
+    calculateAndPrintDistance(StartTime,StopTime)
  
+  
+
+def calculateAndPrintDistance(startTime, stopTime):
     # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
+    TimeElapsed = stopTime - startTime
     # multiply with the sonic speed (34300 cm/s)
     # and divide by 2, because there and back
     distance = (TimeElapsed * 34300) / 2
- 
-    return distance
+    print ("Measured Distance = %.1f cm" % distance)
+
  
 if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+    while True:
+        distance()
+    GPIO.cleanup()
