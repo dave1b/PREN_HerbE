aaad4791ce4eb72597ab9c3f533662ce5b8c84b3
dave1b <57140925+dave1b@users.noreply.github.com>
2021-12-02 09:47:48 +0100

    initial version of socket on server

--- REST_API/server.js
+++ REST_API/server.js
@@ -1,64 +1,79 @@
+
+var url = "mongodb://localhost:27016/";
+var port = 8080
+
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27016/";
+app.use(express.urlencoded({ extended: false }));
+var io = require("socket.io")(port);
+
+
+io.on("connection", socket => {
+   //either with send()
+   socket.send("Hello!");
+
+   // or with emit() and custom event names
+   socket.emit("greetings", "Hey!", { "ms": "jane" }, Buffer.from([4, 3, 3, 1]));
+
+   // handle the event sent with socket.send()
+   socket.on("message", (data) => {
+      console.log(data);
+   });
+
+   // handle the event sent with socket.emit()
+   socket.on("salutations", (elem1, elem2, elem3) => {
+      console.log(elem1, elem2, elem3);
+   });
+});
 
 
 // Client requesting all users
-
-app.get('/listUsers', function (req, res) {
-  
+app.get('/list', (req, res) => {
+   console.log("get: list")
+   res.send(getLatestRunFromDB())
 });
 
-// Client requesting a specific user
-app.get('/:id', function (req, res) {
-   
+app.post('/updateRun', (req, res) => {
+   console.log("post: udpateRun")
+   updateRunInDB(req.body)
+   res.send('Message received')
+   console.log(req.body)
 });
- // example request: http://127.0.0.1:8080/2 and
 
-// Client adding new User
-app.post('/addUser', function (req, res) {
-
-   var myObjec = JSON.parse(req);
-   console.log(myObjec)
-   console.log("PostRequest")
-
-   /*
-   var run = [
-      { name: 'John', address: 'Highway 71'},
-      { name: 'Peter', address: 'Lowstreet 4'},
-      { name: 'Amy', address: 'Apple st 652'},
-      { name: 'Hannah', address: 'Mountain 21'},
-      { name: 'Michael', address: 'Valley 345'},
-      { name: 'Sandy', address: 'Ocean blvd 2'},
-      { name: 'Betty', address: 'Green Grass 1'},
-      { name: 'Richard', address: 'Sky st 331'},
-      { name: 'Susan', address: 'One way 98'},
-      { name: 'Vicky', address: 'Yellow Garden 2'},
-      { name: 'Ben', address: 'Park Lane 38'},
-      { name: 'William', address: 'Central st 954'},
-      { name: 'Chuck', address: 'Main Road 989'},
-      { name: 'Viola', address: 'Sideway 1633'}
-    ];
-    console.log(run)
-
-   MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("prenn");
-      dbo.collection("runs").insertMany(run, function(err, res) {
-         if (err) throw err;
-         
-         db.close();
-      });
-      });
-
-      */
-}); 
-   
-
-var server = app.listen(8080, function () {
+var server = app.listen(port, function () {
    //var host = "prenh21-dbrunner.enterpriselab.ch"
    var host = server.address().address;
    var port = server.address().port;
    console.log("Example app listening at http://%s:%s", host, port)
 });
+
+
+function updateRunInDB(run) {
+   MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("pren");
+      var query = { id: "1" };
+      dbo.collection("run").updateOne(updateDocWhere, { $set: run }, function (err, res) {
+         if (err) throw err;
+         console.log("1 document inserted");
+         db.close();
+      });
+   });
+   return
+};
+
+function getLatestRunFromDB() {
+   MongoClient.connect(url, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("pren");
+      var query = { id: "1" };
+      dbo.collection("run").find(query).toArray(function (err, result) {
+         if (err) throw err;
+         db.close();
+         console.log(result);
+         return (result)
+      });
+   });
+}
+
