8e5438ef8539bcbbe57d0a414bf875aca12730a5
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-02-25 17:22:09 +0100

    initial commit of vue.js website

---
+++ website_pren/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Panel from "./components/Panel.vue";
+
+
+</script>
+
+<template>
+<h1>Servus</h1>
+<Panel />
+  
+</template>
+
+<style>
+@import './assets/base.css';
+@import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+@import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+
+
+</style>
---
+++ website_pren/src/components/Panel.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Hello There</h1>
+  <h1>{{ count }}</h1>
+  <div>
+    <button type="button" class="btn btn-primary" v-on:click="increment()" >{{btnName}}</button>
+  </div>
+  <div v-html="btnName"></div>
+
+</template>
+
+<script>
+export default {
+  name: "Panel",
+  data() {
+    return {
+      count: 1,
+      btnName: "Lets go and count!"
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+
+  mounted() {
+    this.count = 0;
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
