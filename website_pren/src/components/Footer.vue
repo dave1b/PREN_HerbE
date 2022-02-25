017a1c0c15af2f38f65d2d0c7c99cd25d2cb7aec
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-02-25 19:20:07 +0100

    added footer

---
+++ website_pren/src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer>
+    <br />
+    <div class="middle">
+      <p>Team 40 - HSLU - PREN 2022
+          <br>
+        Dave Brunner, Rachel Sprecher, Emanuel Wyer, Philip Seiler, Tim
+        Christen, Tomas Prajzler, Denis Saric
+      </p>
+      <a
+        class="footer-btn"
+        href="https://www.youtube.com/channel/UCtmv4LU7WXBFtNVOQaqm8TQ"
+      >
+        <i class="fab fa-youtube"></i>
+      </a>
+      <a
+        class="footer-btn"
+        href="https://www.instagram.com/sportteam_bbzg_w_sursee/"
+      >
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a class="footer-btn" href="https://www.linkedin.com/in/dave-b-a7a158199">
+        <i class="fab fa-linkedin-in"></i>
+      </a>
+      <a class="footer-btn" href="mailto:dave.brunner@protonmail.ch">
+        <i class="fa-solid fa-envelope"></i>
+      </a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
