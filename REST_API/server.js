c453b3259062ee6a61afda3f1253dd0729da7a32
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-02-25 10:28:35 +0100

    refactoring

--- REST_API/server.js
+++ REST_API/server.js
@@ -1,18 +1,6 @@
-
-var url = "mongodb://localhost:27016/";
-var port = 8080
-
-var express = require('express');
-var app = express();
-
+// SocketConnection
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-
-
-var MongoClient = require('mongodb').MongoClient;
-app.use(express.urlencoded({ extended: false }));
-
-
 
 io.on("connection", socket => {
    //either with send()
@@ -32,73 +20,57 @@ io.on("connection", socket => {
    });
 });
 
+// REST-API
+var MongoClient = require('mongodb').MongoClient;
+var express = require('express');
+var server = express();
+server.use(bodyParser.json());
+server.use(express.urlencoded({ extended: false }));
+
+//Handle CORS-Restriction
+server.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', '*');
+   if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'POST, GET');
+      return res.status(200).json({});
+   }
+   next();
+})
+var connectionString = "mongodb://localhost:27016/";
+var port = 8080
+
 
 // Client requesting all users
-app.get('/list', (req, res) => {
-   
-   MongoClient.connect(url, function (err, db) {
-      if (err) throw err;
-      var dbo = db.db("pren");
-      var query = { id: "1" };
-      dbo.collection("run").find(query).toArray(function (err, result) {
-         if (err) throw err;
-         res.end(JSON.stringify(result[0]))
-         db.close();
-      });
-   });
-   /*
-   console.log("get: list")
-   var json = getLatestRunFromDB()
-   console.log(json)
-   res.end(json)*/
+server.get('/list', (req, res) => {
+   const client = await mongoClient.connect(connectionString);
+   const db = client.db('pren');
+   const collection = db.collection('run');
+   var query = { id: "1" };
+   const result = await collection.find({ query }).toArray();
+   res.json(result)
 });
 
-app.post('/updateRun', (req, res) => {
-   console.log("post: udpateRun")
-   updateRunInDB(req.body)
-   res.send('Message received')
-   console.log(req.body)
-});
-
-
-
-
-function updateRunInDB(run) {
-   MongoClient.connect(url, function (err, db) {
-      if (err) throw err;
-      var dbo = db.db("pren");
-      var query = { id: "1" };
-      dbo.collection("run").updateOne(query, { $set: run }, function (err, res) {
-         if (err) throw err;
-         console.log("1 document inserted");
-         db.close();
-      });
-   });
+server.post('/updateRun', (req, res) => {
+   const client = await mongoClient.connect(connectionString);
+   const db = client.db('pren');
+   const collection = db.collection('run');
+   const queryFilter = { id: "1" }
+   const result = await collection.updateOne(queryFilter, { $set: run });
+   if (result) {
+      res.send(result);
+   } else {
+      res.status(404);
+   }
+   res.end();
    return
-};
+});
 
-function getLatestRunFromDB() {
-   var json = ""
-   MongoClient.connect(url, function (err, db) {
-      if (err) throw err;
-      var dbo = db.db("pren");
-      var query = { id: "1" };
-      dbo.collection("run").find(query).toArray(function (err, result) {
-         if (err) throw err;
-         json = result[0]
-         console.log(json)
-         db.close();
-      });
-   });
-   //console.log(json)
-   return (json)
-}
+var listener = server.listen(port);
+console.log("Server running at port " + listener.address().port)
 
-
-
-
-http.listen(process.env.PORT || port, function() {
+/*http.listen(process.env.PORT || port, function () {
    var host = http.address().address
    var port = http.address().port
    console.log('App listening at http://%s:%s', host, port)
- });
+});*/
