25778056dc6fc172e05b53f10950c0f4d50ba4a6
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-02-25 19:20:13 +0100

    added header

---
+++ website_pren/src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          <img
+            src="../assets/herb-e.png"
+            width="100"
+            style="margin-top: -5px"
+          />
+          &nbsp; &nbsp; Herb-E
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active-link">
+              <a class="nav-link" style="margin-bottom: 5px" href="https://www.google.ch">PREN Team 40</a>
+            </li>
+          </ul>
+        </div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
