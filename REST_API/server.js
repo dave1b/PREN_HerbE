d2cd1e36ae8977e1a1821754976708841f98cc0f
dave1b <57140925+dave1b@users.noreply.github.com>
2022-03-03 22:29:57 +0100

    added authentification for post-request

--- REST_API/server.js
+++ REST_API/server.js
@@ -1,26 +1,19 @@
-
-
 // REST-API
-var MongoClient = require('mongodb').MongoClient;
+var mongoClient = require('mongodb').MongoClient;
 var express = require('express');
-var bodyParser = require("body-parser");
 var server = express();
-
-require('dotenv').config()
+var bodyParser = require("body-parser");
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
-server.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Headers', '*');
-   if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'POST, GET');
-      return res.status(200).json({});
-   }
-   next();
-})
 
-server.use(bodyParser.json());
-server.use(express.urlencoded({ extended: false }));
+
+
+
+
+
+require('dotenv').config()
+const API_KEY = process.env.SECRET_KEY;
+const CONNECTION_STRING = "mongodb://localhost:27016/";
 
 //Handle CORS-Restriction
 server.use((req, res, next) => {
@@ -32,34 +25,45 @@ server.use((req, res, next) => {
    }
    next();
 })
-var connectionString = "mongodb://localhost:27016/";
 
 
 // Client requesting all users
 server.get('/list', async (req, res) => {
-   const client = await mongoClient.connect(connectionString);
+   const client = await mongoClient.connect(CONNECTION_STRING);
    const db = client.db('pren');
    const collection = db.collection('run');
-   var query = { id: "1" };
-   const result = await collection.find({ query }).toArray();
+   const queryFilter = {id: "1"};
+   const result = await collection.find(queryFilter).toArray();
    res.json(result)
 });
 
+
+// Update run
 server.post('/updateRun', async (req, res) => {
-   const client = await mongoClient.connect(connectionString);
+   const run = req.body;
+
+   // Check validity of Key
+   if (run.apiKey != API_KEY) {
+      res.status(401);
+      res.end();
+      console.log("not authorized")
+      return;
+   }
+   // Write to DB
+   const client = await mongoClient.connect(CONNECTION_STRING);
    const db = client.db('pren');
    const collection = db.collection('run');
-   const queryFilter = { id: "1" }
-   const run = req.body
+   const queryFilter = { id: "1"  }
    const result = await collection.updateOne(queryFilter, { $set: run });
+   // Copy to Archive if end
    if (run.isFinisched == true) {
-      const collection = db.collection('runArchive');
-      await collection.insertOne(run);
+      const collection2 = db.collection('runArchive');
+      await collection2.insertOne(run);
    }
    if (result) {
-      res.send(result);
+       res.send(result);
    } else {
-      res.status(404);
+       res.status(404);
    }
    res.end();
    return
