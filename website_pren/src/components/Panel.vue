57b73cc735e87b08a4133c90e14f913f72a5877b
dave1b <57140925+dave1b@users.noreply.github.com>
2022-03-06 16:51:00 +0100

    implemented socket-connection

--- website_pren/src/components/Panel.vue
+++ website_pren/src/components/Panel.vue
@@ -2,64 +2,124 @@
   <div class="center-container">
     <div class="card">
       <div class="card-body">
-        <h1 class="card-title">Aktulle Informationen</h1>
+        <h1 class="card-title">Aktuelle Informationen</h1>
 
         <div class="inCard-container">
-          <table class="table table-success table-striped">
+          <table class="table table-dark table-striped ">
             <tbody>
               <tr>
                 <td>Startzeitpunkt</td>
-                <td>1</td>
+                <td>{{ startZeitDatum }}</td>
               </tr>
               <tr>
                 <td>Laufzeit</td>
-                <td>1</td>
+                <td>{{ laufzeit }}</td>
               </tr>
               <tr>
                 <td>Distanz</td>
-                <td>2</td>
+                <td>{{ distanz }} Meter</td>
               </tr>
               <tr>
                 <td>Zustand</td>
-                <td>3</td>
+                <td>{{ zustand }}</td>
               </tr>
               <tr>
                 <td>Erkannte Pflanzenart</td>
-                <td>3</td>
+                <td>{{ erkenntePflanze }}</td>
               </tr>
               <tr>
                 <td>Position der gleichen Pflanze</td>
-                <td>3</td>
+                <td>{{ positionDerGleichenPflanze }}</td>
               </tr>
             </tbody>
           </table>
         </div>
-
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h3>Fortschritt von Herb-E</h3>
+        <h4 style="float: left">| Start</h4>
+        <h4 style="float: right"> Ende |</h4>
+        <div class="progress" style="height: 20px; clear: both;">
+          <div
+            class="progress-bar"
+            :class="{ 'progress-bar-striped progress-bar-animated bg-info': !finished, 'bg-success': finished  }"
+            role="progressbar"
+            :style="{ width: distanz + '%' }"
+            aria-valuenow="25"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></div>
+          <img src="../assets/herb-e_mirror.png" width="50" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { io } from "socket.io-client";
 export default {
   name: "Panel",
   data() {
     return {
-      count: 1,
-      btnName: "Lets go and count!",
+      socket: io("http://prenh21-dbrunner.enterpriselab.ch:8080", {
+        transports: ["websocket"],
+      }),
+      timer: undefined,
+      isConnected: false,
+      startZeitDatum: "",
+      startZeitStamp: null,
+      endZeitStamp: null,
+      laufzeit: "",
+      distanz: "",
+      zustand: "",
+      erkenntePflanze: "",
+      positionDerGleichenPflanze: "",
     };
   },
   methods: {
-    increment() {
-      this.count++;
+    start() {
+      this.timer = setInterval(() => {
+        this.updateLaufzeit(Date.now());
+      }, 1000);
+    },
+    stop() {
+      clearInterval(this.timer);
+    },
+    updateLaufzeit(zeitStamp) {
+      var laufzeitInMilli = Math.floor(
+        (zeitStamp - this.startZeitStamp) / 1000
+      );
+      this.laufzeit =
+        Math.floor(laufzeitInMilli / 60).toString() +
+        " Minuten " +
+        (laufzeitInMilli % 60).toString() +
+        " Sekunden";
     },
   },
-
   mounted() {
-    this.count = 0;
+    this.socket.on("runUpdated", (run) => {
+      this.startZeitDatum = run.dateTimeStamp;
+      this.startZeitStamp = run.startTimeStamp;
+      this.endZeitStamp = run.endTimeStamp;
+      this.distanz = run.distance;
+      this.zustand = run.state;
+      this.erkenntePflanze = run.plantType;
+      this.positionDerGleichenPflanze = run.plantMatchPosition;
+      this.finished = run.isFinished;
+      if (this.endZeitStamp > 0 && this.finished == true) {
+        this.updateLaufzeit(this.endZeitStamp);
+        this.stop();
+      } else {
+        this.updateLaufzeit(Date.now());
+        if (this.timer == undefined) {
+          this.start();
+        }
+      }
+    }),
+      this.socket.on("connect", () => {
+        console.log("Successfully connected!");
+      });
   },
+  created() {},
 };
 </script>
-
 <style scoped></style>
