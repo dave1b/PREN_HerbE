59d596919d05a0d7dea640e018170cab8e2ff00e
dave1b <57140925+dave1b@users.noreply.github.com>
2022-03-06 16:50:21 +0100

    removed <br>

--- website_pren/src/App.vue
+++ website_pren/src/App.vue
@@ -2,40 +2,18 @@
 import Panel from "./components/Panel.vue";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
-
-
-
 </script>
 
 <template>
-
 <Header />
 <br>
 <Panel />
 <br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 <Footer />
-  
 </template>
 
 <style>
 @import './assets/base.css';
 @import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 @import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
-
-
 </style>
