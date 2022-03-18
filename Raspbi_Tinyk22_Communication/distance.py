3e67eaa38a64e086abf4be50f1010ea6ae149bfa
Dionysos64 <denissaric19@gmail.com>
2022-03-18 09:55:37 +0100

    Signed-off-by: Dionysos64 <denissaric19@gmail.com>

--- Raspbi_Tinyk22_Communication/distance.py
+++ Raspbi_Tinyk22_Communication/distance.py
@@ -3,40 +3,39 @@ import sys
 import tinyk22_con
 import time
 
-
 class distance:
 
-    def setdistance(self):
-
-        con = tinyk22_con.tinyk22_con()
-
-        ser = con.getconnection()
-
-
-        while True:
-            serialdata=ser.readline()          #take comment out when testing
-            #serialdata = ser                   #for test causes
-
-
-            #read serial data and print it on screen
-            if "distance" in serialdata:
-                print(serialdata)
-
-
-            #Timer, how fast the velocity is gonna be updated
-            time.sleep(2)
+    def __init__(self, ser):
+        self.ser = ser
 
 
     def getdistance(self):
-        return distance;
+        try:
+            ser = self.ser
+
+
+            writeinput = (bytes("distance" + "\n", 'UTF-8'))
+            ser.write(writeinput)
+
+
+            readline = ser.readline().decode('utf-8')
+
+            print('Distanz: ' + readline)
+
+
+        finally:
+            ser.close()
 
 
 
 ## Main method for test reasons
 #def main():
-#    d1 = distance()
-#    d1.setdistance()
-#    print(d1.getdistance())
+#    con = tinyk22_con.tinyk22_con()
+
+#    ser = con.getconnection()
+
+#    d1 = distance(ser)
+#    d1.getdistance()
 
 #    sys.exit()
 
--- Raspbi_Tinyk22_Communication/tinyk22_con.py
+++ Raspbi_Tinyk22_Communication/tinyk22_con.py
@@ -3,7 +3,7 @@ import serial
 class tinyk22_con:
 
 	def getconnection(self):
-		return serial.Serial('/dev/ttyAMA0', 57600)  #try ttyAMA0 and ttyS0 // ttyACM0
+		return serial.Serial('COM3', 57600)  #try ttyAMA0 and ttyS0 // ttyACM0
 
 		#return "velocity: 88mph"
 		#return "distance: 420m"
