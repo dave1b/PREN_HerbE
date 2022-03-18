155ccca6fc8ca32da87400878443034a5962fc60
Dionysos64 <denissaric19@gmail.com>
2022-03-18 09:57:54 +0100

    changed config file
    Signed-off-by: Dionysos64 <denissaric19@gmail.com>

--- Raspbi_Tinyk22_Communication/tinyk22_con.py
+++ Raspbi_Tinyk22_Communication/tinyk22_con.py
@@ -3,7 +3,9 @@ import serial
 class tinyk22_con:
 
 	def getconnection(self):
-		return serial.Serial('COM3', 57600)  #try ttyAMA0 and ttyS0 // ttyACM0
+
+		return serial.Serial('ttyAMA0', 57600)# try ttyAMA0 and ttyS0 // ttyACM0
+		#return serial.Serial('COM3', 57600)  #for notebook tests
 
 		#return "velocity: 88mph"
 		#return "distance: 420m"
