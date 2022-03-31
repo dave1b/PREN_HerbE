929087407665e0f77c28bf0ea7f78540ceaa680d
Brunner Dave dbrunner <dave_brunner@outlook.com>
2022-03-31 23:43:17 +0200

    new view

---
+++ website_pren/src/views/Team.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TeamInfo from '../components/TeamInfo.vue';
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+</script>
+
+<template>
+<Header />
+<br>
+<TeamInfo />
+<br>
+<Footer />
+</template>
+
+<style>
+@import '.././assets/base.css';
+@import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+@import '../../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+</style>
