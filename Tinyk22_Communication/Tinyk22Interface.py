085dc32a5330fe1ace999dd5f6056edd5f6899ac
dave1b <57140925+dave1b@users.noreply.github.com>
2022-03-31 21:17:30 +0200

    added intervall for reading distance

--- Tinyk22_Communication/Tinyk22Interface.py
+++ Tinyk22_Communication/Tinyk22Interface.py
@@ -1,47 +1,36 @@
-import time
-
 from Distance import Distance
 from Engine import Engine
 from Tinyk22Con import Tinyk22Con
 import threading
 
-
-
 class Tinyk22Interface:
     def __init__(self, newDistanceCallback):
         self.ser = Tinyk22Con.getconnection()
         self.engineRunning = False
-        self.engine = Engine(ser)
-        self.distance = Distance(ser)
-
+        self.engine = Engine(self.ser)
+        self.distance = Distance(self.ser)
+        self.timerInverval = 5
         self.newDistanceCallback = newDistanceCallback
-
-        self._timer = None
-        self.interval = 5
-
 
     def turnEngineOn(self):
         self.engine.engineOn(self.ser)
         self.engineRunning = True
-        setIntervalTime()
+        self.startIntervalTime()
 
     def turnEngineOff(self):
         self.engine.engineOff(self.ser)
         self.engineRunning = True
-        setIntervalTime()
 
     def receiveDistanceAndCallCallback(self):
         distance = self.distance.getDistance(self.ser)
         self.newDistanceCallback(distance)
 
-    def setIntervalTime(self):
-        thread = threading.Timer(interval, self.receiveDistanceAndCallCallback())
-
-        if self.engineRunning == true:
-            #self._timer = Timer(self.interval, self.receiveDistanceAndCallCallback())
-            #starttime = threading.Timer(interval, self.receiveDistanceAndCallCallback())
-            thread.start()
-
-        else:
-            thread.stop()
-
+    def startIntervalTime(self):
+        def func_wrapper(self):
+            if(self.engineRunning):
+                self.set_interval(self.receiveDistanceAndCallCallback(), self.timerInverval)
+                self.receiveDistanceAndCallCallback()
+            else:
+                return
+        t = threading.Timer(self.timerInterval, func_wrapper)
+        t.start()
