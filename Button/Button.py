60589a8f7db6b0075a8237d1598fe1337a3bccdd
dave1b <57140925+dave1b@users.noreply.github.com>
2022-03-31 21:15:12 +0200

    button for reading press-input

---
+++ Button/Button.py
@@ -0,0 +1,22 @@
+from mimetypes import init
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+class Button():
+    def __init__(self, CallbackFunctionFirstTimePressed, CallbackFunctionXTimesPressd):
+        self.CallbackFunctionFirstTimePressed = CallbackFunctionFirstTimePressed
+        self.CallbackFunctionXTimesPressd = CallbackFunctionXTimesPressd
+        self.pressedBefore = False
+
+    def startButtonListener(self):
+         while True: 
+            if GPIO.input(10) == GPIO.HIGH:
+                print("Button was pushed!")
+                if not(self.pressedBefore):
+                    self.CallbackFunctionFirstTimePressed()
+                    self.pressedBefore = True
+                else:
+                    self.CallbackFunctionXTimesPressd()            
