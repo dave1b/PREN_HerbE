4a6755cef8a55983af3615815e6a5ae64d15fdc0
dave1b <57140925+dave1b@users.noreply.github.com>
2022-03-31 08:59:51 +0200

    wrapper for ultrasonic_library

---
+++ Ultrasonic_Waterproof/Ultrasonic.py
@@ -0,0 +1,23 @@
+import time
+from DFRobotRaspberryPiA02YYUW import DFRobotA02Distance as UltrasonicSensor
+
+class Ultrasonic:
+    def __init__(self, objectDetected, dataModel, distanceThreshold = 1000):
+        self.callbackObjectDetected = objectDetected
+        self.dataModel = dataModel
+        self.ultrasonicSensor = UltrasonicSensor()
+        self.ultrasonicSensor.set_dis_range(dis_min=0, dis_max=3000)
+        self.distanceThreshold = distanceThreshold
+        self.serachingRunning = False
+    
+    def startSearching(self):
+        self.startSearching = True
+        while self.serachingRunning:
+            distance = self.ultrasonicSensor.getDistance()
+            if distance <= self.distanceThreshold:
+                self.callbackObjectDetected()
+                self.dataModel.UltrasonicObjectDetected = True
+            time.sleep(0.3)    
+    
+    def stopSearching(self):
+        self.serachingRunning = False
