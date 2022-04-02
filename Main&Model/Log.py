3c6e5dd0f71fb4048a94ae420876ff73aa8b3e36
Brunner Dave dbrunner <dave_brunner@outlook.com>
2022-04-03 00:03:37 +0200

    Logger class

--- Main&Model/Log.py
+++ Main&Model/Log.py
@@ -1,17 +1,26 @@
-
-
 import logging
-logger = logging.getLogger('HerbE-Logger')
-logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler('HerbE.log')
-fh.setLevel(logging.DEBUG)
-logger.addHandler(fh)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-logger.addHandler(fh)
+import sys
 
-#logger.debug('DebugNachricht')
-#logger.info('InfoNachricht')
-#logger.warning('Warnhinweis')
-#logger.error('Fehlermeldung')
-#logger.critical('Schwerer Fehler')
+class Logger:
+    def __init__(self):
+        logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] ---- %(message)s",
+        handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler(sys.stdout)
+        ])
+        
+    def debug(self, message):
+        logging.debug(message)
+    def info(self, message):
+        logging.info(message)
+    def warning(self, message):
+        logging.warning(message)
+    def error(self, message):
+        logging.error(message)
+    
+#logging.debug('This message is skipped as a level is set as INFO')
+#logging.info('So should this')
+#logging.warning('And this, too')
+#logging.error('Testing non-ASCII character, Ø and ö')
