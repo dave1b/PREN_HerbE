3d17c3285a9843881bbf6a353ccf2527dbe98e27
Brunner Dave dbrunner <dave_brunner@outlook.com>
2022-04-04 23:37:02 +0200

    added grid system

--- website_pren/src/components/Footer.vue
+++ website_pren/src/components/Footer.vue
@@ -1,30 +1,53 @@
 <template>
   <footer>
-    <br />
     <div class="middle">
-      <p>Team 40 - HSLU - PREN 2022
-          <br>
-        Dave Brunner, Rachel Sprecher, Emanuel Wyer, Philip Seiler, Tim
-        Christen, Tomas Prajzler, Denis Saric
-      </p>
-      <a
-        class="footer-btn"
-        href="https://www.youtube.com/channel/UCtmv4LU7WXBFtNVOQaqm8TQ"
-      >
-        <i class="fab fa-youtube"></i>
-      </a>
-      <a
-        class="footer-btn"
-        href="https://www.instagram.com/sportteam_bbzg_w_sursee/"
-      >
-        <i class="fab fa-instagram"></i>
-      </a>
-      <a class="footer-btn" href="https://www.linkedin.com/in/dave-b-a7a158199">
-        <i class="fab fa-linkedin-in"></i>
-      </a>
-      <a class="footer-btn" href="mailto:dave.brunner@protonmail.ch">
-        <i class="fa-solid fa-envelope"></i>
-      </a>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <h7>Team 40 - HSLU - PREN 2022</h7>
+            <ul style="list-style: none">
+              <li>Dave Brunner</li>
+              <li>Rachel Sprecher</li>
+              <li>Emanuel Wyer</li>
+              <li>Philip Seiler</li>
+              <li>Tomas Prajzler</li>
+              <li>Denis Saric</li>
+            </ul>
+          </div>
+          <div class="col-sm">
+            <h7>Social Media</h7>
+            <div class="">
+              <a
+                class="footer-btn"
+                href="https://www.youtube.com/channel/UCtmv4LU7WXBFtNVOQaqm8TQ"
+              >
+                <i class="fab fa-youtube"></i>
+              </a>
+              <a
+                class="footer-btn"
+                href="https://www.instagram.com/sportteam_bbzg_w_sursee/"
+              >
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a
+                class="footer-btn"
+                href="https://www.linkedin.com/in/dave-b-a7a158199"
+              >
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+              <a class="footer-btn" href="mailto:dave.brunner@protonmail.ch">
+                <i class="fa-solid fa-envelope"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-sm">
+            <h7>Hochschule</h7>
+            <div>
+              <img src="../assets/hslu_logo.svg" style="padding-top: 15px" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
@@ -39,4 +62,11 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+h7 {
+  font-size: larger;
+  font-weight: bold;
+  text-decoration: underline 2px;
+  text-underline-offset: 5px;
+}
+</style>
