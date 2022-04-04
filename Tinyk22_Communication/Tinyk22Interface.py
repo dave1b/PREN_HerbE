d7ed464c69fdb4940bab1dc9aa8bb0d5fdf47ca6
Brunner Dave dbrunner <dave_brunner@outlook.com>
2022-04-04 09:09:27 +0200

    fixed interval and added logging

--- Tinyk22_Communication/Tinyk22Interface.py
+++ Tinyk22_Communication/Tinyk22Interface.py
@@ -1,7 +1,10 @@
 from Distance import Distance
 from Engine import Engine
 from Tinyk22Con import Tinyk22Con
-import threading
+from threading import Timer
+import sys
+sys.path.insert(0, '../Main&Model')
+from Log import Logger
 
 class Tinyk22Interface:
     def __init__(self, newDistanceCallback):
@@ -9,28 +12,37 @@ class Tinyk22Interface:
         self.engineRunning = False
         self.engine = Engine(self.ser)
         self.distance = Distance(self.ser)
-        self.timerInverval = 5
+        self.timerInterval = 1
+        self.thread = Timer(self.timerInterval, self.func_wrapper)
         self.newDistanceCallback = newDistanceCallback
+        self.log = Logger()
 
     def turnEngineOn(self):
+        self.log.debug("Tinyk22 Interface: turnEngineOn()")
         self.engine.engineOn(self.ser)
         self.engineRunning = True
         self.startIntervalTime()
 
-    def turnEngineOff(self):
+    def turnEngineOff(self):        
+        self.log.debug("Tinyk22 Interface: turnEngineOff()")
         self.engine.engineOff(self.ser)
         self.engineRunning = True
 
     def receiveDistanceAndCallCallback(self):
+        self.log.debug("Tinyk22 Interface: receiveDistanceAndCallCallback()")
         distance = self.distance.getDistance(self.ser)
         self.newDistanceCallback(distance)
 
+     def func_wrapper(self):
+            self.log.debug("Tinyk22 Interface: func_wrapper()")
+        if(self.engineRunning):
+            self.log.debug("Tinyk22 Interface: startIntervalTime() if(self.engineRunning)")
+            self.receiveDistanceAndCallCallback()
+            self.thread = Timer(self.timerInterval, self.func_wrapper)
+            self.thread.start()
+        else:
+            return
+
     def startIntervalTime(self):
-        def func_wrapper(self):
-            if(self.engineRunning):
-                self.set_interval(self.receiveDistanceAndCallCallback(), self.timerInverval)
-                self.receiveDistanceAndCallCallback()
-            else:
-                return
-        t = threading.Timer(self.timerInterval, func_wrapper)
-        t.start()
+        self.log.debug("Tinyk22 Interface: startIntervalTime()")
+        self.thread.start()
