80b1c31cbd2bbefd5ddd5438e27cbc361c8795f5
dave1b <57140925+dave1b@users.noreply.github.com>
2022-04-08 08:40:08 +0200

    bug fix

--- Ultrasonic_Waterproof/Ultrasonic.py
+++ Ultrasonic_Waterproof/Ultrasonic.py
@@ -5,7 +5,7 @@ from Log import Logger
 from DFRobotRaspberryPiA02YYUW import DFRobotA02Distance as UltrasonicSensor
 
 class Ultrasonic:
-    def __init__(self, objectDetectedCallback, dataModel, distanceThreshold = 1000):
+    def __init__(self, objectDetectedCallback, dataModel, distanceThreshold = 5000):
         self.callbackObjectDetected = objectDetectedCallback
         self.dataModel = dataModel
         self.ultrasonicSensor = UltrasonicSensor()
@@ -16,15 +16,16 @@ class Ultrasonic:
         self.log.debug("Ultrasonic - initialisiert")
     
     def startSearching(self):
-        self.startSearching = True
+        self.searchingRunning = True
         self.log.debug("Ultrasonic - startSearching()")
         while self.searchingRunning:
             distance = self.ultrasonicSensor.getDistance()
+            self.log.debug("Ultrasonic - meassured Distance:1 " + str(distance))
             if distance <= self.distanceThreshold:
                 self.objectDetectedCallback()
                 self.dataModel.UltrasonicObjectDetected = True
-            self.log.debug("Ultrasonic - meassured Distance: " + str(distance))
-            time.sleep(0.3)    
+            self.log.debug("Ultrasonic - meassured Distance:2 " + str(distance))
+            #time.sleep(0.3)    
     
     def stopSearching(self):
         self.log.debug("Ultrasonic - stopSearching")
