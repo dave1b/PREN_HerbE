21e6151e38db4a5ed711511c1b3befcca1de4d1a
Brunner Dave dbrunner <dave.brunner@stud.hslu.ch>
2022-04-10 16:59:33 +0200

    changed gpio input pin to Nr: 12

--- Button/Button.py
+++ Button/Button.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 class Button():
     def __init__(self, CallbackFunctionFirstTimePressed, CallbackFunctionXTimesPressd):
@@ -13,7 +13,7 @@ class Button():
 
     def startButtonListener(self):
          while True: 
-            if GPIO.input(10) == GPIO.HIGH:
+            if GPIO.input(12) == GPIO.HIGH:
                 print("Button was pushed!")
                 if not(self.pressedBefore):
                     self.CallbackFunctionFirstTimePressed()
