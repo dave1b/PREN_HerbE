a90fa9840302c66fb79dde5de49508807133c01a
dave1b <57140925+dave1b@users.noreply.github.com>
2022-04-10 11:33:56 +0200

    added more logging

--- Ultrasonic_Waterproof/Ultrasonic.py
+++ Ultrasonic_Waterproof/Ultrasonic.py
@@ -9,7 +9,7 @@ class Ultrasonic:
         self.callbackObjectDetected = objectDetectedCallback
         self.dataModel = dataModel
         self.ultrasonicSensor = UltrasonicSensor()
-        self.ultrasonicSensor.set_dis_range(min=0, max=3000)
+        self.ultrasonicSensor.set_dis_range(0, 4500)
         self.distanceThreshold = distanceThreshold
         self.searchingRunning = False
         self.log = Logger()
@@ -22,11 +22,18 @@ class Ultrasonic:
             distance = self.ultrasonicSensor.getDistance()
             self.log.debug("Ultrasonic - meassured Distance:1 " + str(distance))
             if distance <= self.distanceThreshold:
-                self.objectDetectedCallback()
+                self.log.debug("Ultrasonic - meassured Distance:2 " + str(distance))
+                self.callbackObjectDetected()
                 self.dataModel.UltrasonicObjectDetected = True
-            self.log.debug("Ultrasonic - meassured Distance:2 " + str(distance))
+            self.log.debug("Ultrasonic - meassured Distance:3 " + str(distance))
             #time.sleep(0.3)    
     
     def stopSearching(self):
         self.log.debug("Ultrasonic - stopSearching")
         self.searchingRunning = False
+# def hoi():
+#     pass
+# class x:
+#     UltrasonicObjectDetected = ""
+# ultra = Ultrasonic(hoi,x)
+# ultra.startSearching()
