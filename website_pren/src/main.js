260c84a9593a79134fdd58b282d3e2a3b9285c20
dave1b <57140925+dave1b@users.noreply.github.com>
2022-04-14 13:22:25 +0200

    added import of internal bootstrap script

--- website_pren/src/main.js
+++ website_pren/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 createApp(App)
 .use(router)
 .mount('#app')
