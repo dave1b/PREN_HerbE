583bb3046d1ce30c97f75ecdee1c2df3541db36f
dave1b <57140925+dave1b@users.noreply.github.com>
2022-04-14 13:13:20 +0200

    added new components

---
+++ website_pren/src/components/Construction.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="center-container">
+  <h1>Construction</h1>
+  
+</div>
+</template>
+
+<script>
+export default {
+  name: "Construction",
+     
+  created() {},
+};
+</script>
+<style scoped>
+</style>
--- website_pren/src/components/TeamInfo.vue
+++ website_pren/src/components/TeamInfo.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="center-container">
-    <h1>Teaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaam</h1>
+    <h1>Team 40</h1>
+    <div>
+      <br>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <img src="../assets/faceImages/dave.jpg" alt="image of Dave" class="img-thumbnail mx-auto d-block">
+      <h5 class="nameHeading">Dave Brunner</h5>
+    </div>
+    <div class="col-sm">
+<img src="../assets/faceImages/rachel.jpg" alt="image of Dave" class="img-thumbnail mx-auto d-block">    
+      <h5 class="nameHeading">Rachel Sprecher</h5>
+</div>
+    <div class="col-sm">
+<img src="../assets/faceImages/denis.jpg" alt="image of Dave" class="img-thumbnail mx-auto d-block">  
+      <h5 class="nameHeading">Denis Saric</h5>
+  </div>
+  </div>
+  <div class="row">
+    <div class="col-sm">
+<img src="../assets/faceImages/emanuel.jpg" alt="image of Dave" class="img-thumbnail mx-auto d-block">  
+      <h5 class="nameHeading">Emanuel Wyer</h5>
+  </div>
+    <div class="col-sm">
+<img src="../assets/faceImages/tim.jpg" alt="image of Dave" class="img-thumbnail mx-auto d-block">  
+      <h5 class="nameHeading">Tim Christen</h5>
+  </div>
+    <div class="col-sm">
+<img src="../assets/faceImages/tomas.jpg" alt="image of Dave" class="img-thumbnail mx-auto d-block"> 
+      <h5 class="nameHeading">Tomas Prajzler</h5>
+   </div>
+  </div>
+</div>
+
+
+
+    </div>
     </div>
 </template>
 
@@ -15,4 +51,10 @@ export default {
 img.plantIMG{
   width: 90%;
 }
+
+.nameHeading {
+  text-align: center;
+}
+
+
 </style>
