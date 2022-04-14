2cc0d02ab94e5b3a42a5473bbc26993aecf53abd
dave1b <57140925+dave1b@users.noreply.github.com>
2022-04-14 13:13:34 +0200

    added new vue

---
+++ website_pren/src/views/Construction.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Construction from '../components/Construction.vue';
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+</script>
+
+<template>
+
+<Header />
+<br>
+<Construction />
+<br>
+<Footer />
+</template>
+
+<style>
+@import '.././assets/base.css';
+@import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+@import '../../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+</style>
---
+++ website_pren/src/views/Sponsoren.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Sponsoren from '../components/Sponsoren.vue';
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+</script>
+
+<template>
+
+<Header />
+<br>
+<Sponsoren />
+<br>
+<Footer />
+</template>
+
+<style>
+@import '.././assets/base.css';
+@import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+@import '../../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+</style>
