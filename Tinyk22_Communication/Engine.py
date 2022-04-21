da7e6612aec78dea46decb0f398ae2f9901696fa
dsaric <denis.saric@stud.hslu.ch>
2022-04-21 08:59:52 +0200

    Signed-off-by: dsaric <denis.saric@stud.hslu.ch>
    Engine.py fixed -> Distance Output
    
    Signed-off-by: dsaric <denis.saric@stud.hslu.ch>

--- Tinyk22_Communication/Engine.py
+++ Tinyk22_Communication/Engine.py
@@ -1,33 +1,39 @@
 import sys
-
-sys.path.insert(0, '../Main_Model')
+sys.path.insert(0, '../Main&Model')
 from Log import Logger
-
 
 class Engine:
 
     def __init__(self, ser):
-        self.log = Logger()
         self.ser = ser
+        self.log = Logger()
 
     def engineOn(self):
         self.log.debug("Tinyk22 Engine: engineOn()")
+        ser = self.ser
         try:
-            self.updateState('start')
+            self.updateState(ser, 'start')
 
         finally:
             print("")
 
     def engineOff(self):
         self.log.debug("Tinyk22 Engine: engineOff()")
+        ser = self.ser
+
         try:
-            self.updateState('stop')
+            self.updateState(ser, 'stop')
         finally:
             print("")
 
-    def updateState(self, state):
+    def updateState(self, ser, state):
         self.log.debug("Tinyk22 Engine: updateState()")
-
         changeState = (bytes(state + "\n", 'UTF-8'))
-        self.ser.write(changeState)
+        ser.write(changeState)
+
+        readState = ser.readline().decode('utf-8')
+
         print(readState)
+
+
+
