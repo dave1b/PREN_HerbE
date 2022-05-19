df828396596d4812f6615b555b3b0f42200b8e1f
dave1b <57140925+dave1b@users.noreply.github.com>
2022-05-19 09:18:04 +0200

    fix: returning distance

--- Tinyk22_Communication/Distance.py
+++ Tinyk22_Communication/Distance.py
@@ -3,23 +3,22 @@ import sys
 sys.path.insert(0, '/home/pi/Desktop/PREN/Main_Model')
 sys.path.insert(0, '../Main_Model')
 from Log import Logger
+
 class Distance:
 
     def __init__(self, ser):
         self.ser = ser
+        self.log = Logger()
 
     def getDistance(self):
         try:
             ser = self.ser
-
             # sends the distance-request
             request = (bytes('distance\n', 'UTF-8'))
             ser.write(request)
-
             # gets the distance back from Tinyk22
             distance = str(ser.readline(),'utf-8')
-
             self.log.debug("Tinyk22 Distance: getDistance() distance: " + distance)
-
-        finally:
-            #todo: self.log.error
+            return distance
+        except:
+            self.log.error("Tinyk22 Distance - error occured")
