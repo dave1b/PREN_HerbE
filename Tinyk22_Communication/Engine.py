29281ed0eb8913124c2619b693a726cf5cf542c6
dave1b <57140925+dave1b@users.noreply.github.com>
2022-05-20 11:04:58 +0200

    implemented method for definitely shutting

--- Tinyk22_Communication/Engine.py
+++ Tinyk22_Communication/Engine.py
@@ -7,18 +7,20 @@ class Engine:
 
     def __init__(self, ser):
         self.ser = ser
+        self.shutdown= False
         self.log = Logger()
 
     def engineOn(self):
-        self.log.debug("Tinyk22 Engine: engineOn()")
-        try:
-            self.updateState('start')
-        except (RuntimeError):
-            self.log.debug("EngineOn(): RuntimeError")
-        except TypeError:
-            self.log.debug("EngineOn(): TypeError")
-        except NameError:
-            self.log.debug("EngineOn(): NameError")
+        if not(self.shutdown):
+            self.log.debug("Tinyk22 Engine: engineOn()")
+            try:
+                self.updateState('start')
+            except (RuntimeError):
+                self.log.debug("EngineOn(): RuntimeError")
+            except TypeError:
+                self.log.debug("EngineOn(): TypeError")
+            except NameError:
+                self.log.debug("EngineOn(): NameError")
 
     def engineOff(self):
         self.log.debug("Tinyk22 Engine: engineOff()")
@@ -30,8 +32,12 @@ class Engine:
             self.log.debug("EngineOff(): TypeError")
         except NameError:
             self.log.debug("EngineOff(): NameError")
+    def engineShutdown(self):
+        self.log.debug("EngineShutdown()")
+        self.shutdown= True
+        self.engineOff()
 
     def updateState(self, state):
-        self.log.debug("Tinyk22 Engine: updateState()")
+        self.log.debug("Tinyk22 Engine: updateState(): " + state)
         changeState = (bytes(state + "\n", 'UTF-8'))
         self.ser.write(changeState)
