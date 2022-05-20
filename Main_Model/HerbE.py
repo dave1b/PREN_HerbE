117eb90a7dff39f37ce83c287ceea429018b2437
dave1b <57140925+dave1b@users.noreply.github.com>
2022-05-20 11:03:24 +0200

    refactor moved checking timestamp to classes

--- Main_Model/HerbE.py
+++ Main_Model/HerbE.py
@@ -3,6 +3,7 @@ from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 import sys
 import threading
+from threading import Timer
 import time
 import requests
 
@@ -38,14 +39,14 @@ class HerbE:
         self.videoQRCodeScanner = VideoQRCodeScanner(self.qrCodeDetected, self.dataModel)
         self.plantApiService = PlantApiService(self.plantIDKey, self.dataModel, 0.025)
         self.tinyk22Interface = Tinyk22Interface(self.newDistanceCallback)
-        self.lastUltrasonicAlertTimestamp = time.time()
-        self.lastQRcodeDetectedAlertTimestap = time.time()
         self.minWaitingtimeBetweenAlerts = 10
         self.RESTapiURL = "https://prenh21-dbrunner.enterpriselab.ch/api/v1/updateRun"
         self.log = Logger()
         self.log.debug("HerbE - HerbE initialisiert")
 
     def initialStartOfHerbE(self):
+        self.lastUltrasonicAlertTimestamp = time.time()
+        self.lastQRcodeDetectedAlertTimestap = time.time()
         self.log.debug("HerbE - initialStartOfHerbE()")
         self.dataModel.state = HerbEstates["initial"]
         executor = ThreadPoolExecutor(max_workers=3)
@@ -81,25 +82,22 @@ class HerbE:
 
     def newDistanceCallback(self, newDistanceDriven):
         self.log.debug("HerbE - newDistanceCallback()")
-        self.dataModel.distanceDriven = newDistanceDriven
+        self.dataModel.distanceDriven = int(newDistanceDriven)
         self.postDataToRestAPI()
 
     def ultrasonicObjectDetected(self):
-        if (self.isWaitingTimeOver(self.lastUltrasonicAlertTimestamp, self.minWaitingtimeBetweenAlerts)):
-            self.lastUltrasonicAlertTimestamp = time.time()
-            self.log.debug("HerbE - ultrasonicObjectDetected()")
-            self.dataModel.state = HerbEstates["ultraDetected"]
-            self.stopEngine()
-            self.postDataToRestAPI()
-            threading.Timer(5, self.startEngine).start()
+        self.log.debug("HerbE - ultrasonicObjectDetected()")
+        self.dataModel.state = HerbEstates["ultraDetected"]
+        self.stopEngine()
+        self.postDataToRestAPI()
+        Timer(5, self.startEngine).start()
 
     def qrCodeDetected(self):
-        if (self.isWaitingTimeOver(self.lastQRcodeDetectedAlertTimestap, self.minWaitingtimeBetweenAlerts)):
-            self.lastQRcodeDetectedAlertTimestap = time.time()
-            self.log.debug("HerbE - qrCodeDetected()")
-            self.dataModel.state = HerbEstates["qrDetected"]
-            self.videoQRCodeScanner.takePhoto()
-            self.detectPlantInImage()
+        self.lastQRcodeDetectedAlertTimestap = time.time()
+        self.log.debug("HerbE - qrCodeDetected()")
+        self.dataModel.state = HerbEstates["qrDetected"]
+        self.videoQRCodeScanner.takePhoto()
+        self.detectPlantInImage()
 
     def findMatchingPlant(self):
         self.log.debug("HerbE - findMatchingPlant()")
@@ -108,10 +106,6 @@ class HerbE:
         if (didFindMatch):
             self.postDataToRestAPI()
 
-    def isWaitingTimeOver(self, lastAlertTimestamp, waitingThreshold):
-        self.log.debug("HerbE - isWaitingTimeOver()")
-        return (((time.time()) - lastAlertTimestamp) > waitingThreshold)
-
     def postDataToRestAPI(self):
         self.log.debug("HerbE - postDataToRestAPI()")
         response = (requests.put(self.RESTapiURL, json=self.dataModel.toJSON(restAPIKey))).status_code
@@ -119,7 +113,7 @@ class HerbE:
 
     def shutdownHerbE(self):
         self.log.debug("HerbE - shutdownHerbE()")
-        self.stopEngine()
+        self.tinyk22Interface.shutdownEngine()
         self.videoQRCodeScanner.stop()
         self.ultrasonic.stopSearching()
         self.dataModel.state = HerbEstates["finished"]
