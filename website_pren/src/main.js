bd17c0f6976f7e71398017b9db0e2716c7f0f094
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-06-06 18:30:52 +0200

    removed code to own repository

--- website_pren/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from '@/router'
-
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-createApp(App)
-.use(router)
-.mount('#app')
