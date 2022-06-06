70e4f2041a4945a0596f7af0f7589f6b421a79ae
Brunner Dave dbrunner <57140925+dave1b@users.noreply.github.com>
2022-06-06 17:45:10 +0200

    empty string

--- Main_Model/configGenerator.py
+++ Main_Model/configGenerator.py
@@ -9,15 +9,15 @@ config_object["HERBECONFIG"] = {
     "plantMinProbability": 0.025,
     "restAPIURL": "https://prenh21-dbrunner.enterpriselab.ch/api/v1/updateRun",
     "qrContentOfFinish": "Ziel",
-    "afterUltraDetectedRestartHerbETimeInSeconds": 1.5,
+    "afterUltraDetectedRestartHerbETimeInSeconds": 1,
     "shutdownAfterFinishQRcodeTimeInSeconds": 5,
     "plantImgPath": "/home/pi/plantImage.png",
     "cameraContrast": 20,
     "camerExposure": 40,
     "getDistanceIntervalTimeInSeconds": 1,
     "ultrasonicTimeThresholdBetweenInSeconds": 12,
-    "plantIDkey": "secret",
-    "restAPIKey": "secret"
+    "plantIDkey": "",
+    "restAPIKey": ""
 }
 
 # Write the above sections to config.ini file
